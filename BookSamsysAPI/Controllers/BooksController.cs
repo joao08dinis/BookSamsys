12fcf57effff43eef3b1e83c8701fedea1f9a9fa
Joao Laptop <97002278+joao08dinis@users.noreply.github.com>
2024-03-15 22:15:28 +0000

    Update Controller
    Add delete method for Controller and comment all methods on controller

--- BookSamsysAPI/Controllers/BooksController.cs
+++ BookSamsysAPI/Controllers/BooksController.cs
@@ -17,10 +17,20 @@ namespace BookSamsysAPI.Controllers
         {
             this.dbContext = dbContext;
         }
+
+        // GET: api/books
         [HttpGet]
         public async Task<IActionResult> GetBooks()
         {
-            return Ok(await dbContext.Books.ToListAsync());
+            //Get all books from DB
+            var books = await dbContext.Books.ToListAsync();
+            
+            //If no book was found return an error
+            if(books.Count == 0)  
+                return NotFound(new Error("Books Not Found", $"No book was found."));
+            
+            //Return all books from DB
+            return Ok(books);
         }
 
 
@@ -28,15 +38,19 @@ namespace BookSamsysAPI.Controllers
         [HttpGet("isbn/{ISBN}")]
         public async Task<IActionResult> GetBookByISBN(string ISBN)
         {
+            //If the ISBN has not 13 digits return errpr
             if (ISBN.Length != 13) return BadRequest(new Error("Wrong ISBN", "The ISBN must has 13 digits"));
 
+            //Get all books from DB
             var dbBooks = await dbContext.Books.ToListAsync();
 
+            //Find the book with that ISBN and return it
             foreach (var book in dbBooks)
             {
                 if (book.ISBN == ISBN) return Ok(book);
             }
 
+            //If no book was found return an error
             return NotFound(new Error("Book Not Found", $"No book was found with this ISBN: {ISBN}."));
         }
 
@@ -45,16 +59,22 @@ namespace BookSamsysAPI.Controllers
         [HttpGet("author/{Author}")]
         public async Task<IActionResult> GetBookByAuthor(string Author)
         {
+            //Get all books from DB
             var dbBooks = await dbContext.Books.ToListAsync();
 
+            //Create an arrayList of books
             ArrayList books = new ArrayList();
 
+            //Add to the arrayList all books with that Author
             foreach (var book in dbBooks)
             {
                 if (book.Author == Author) books.Add(book);
             }
 
+            //If no book was found return error
             if (books.Count == 0) return NotFound(new Error("Book Not Found", $"No book was found with this Author: {Author}."));
+
+            //Otherwise, return the book
             return Ok(books);
         }
 
@@ -62,16 +82,22 @@ namespace BookSamsysAPI.Controllers
         [HttpGet("name/{Name}")]
         public async Task<IActionResult> GetBookByName(string Name)
         {
+            //Get all books from DB
             var dbBooks = await dbContext.Books.ToListAsync();
 
+            //Create an arrayList of books
             ArrayList books = new ArrayList();
 
+            //Add to the arrayList all books with that Name
             foreach (var book in dbBooks)
             {
                 if (book.Name == Name) books.Add(book);
             }
 
-            if (books == null) return NotFound(new Error("Book Not Found", $"No book was found with this Name: {Name}."));
+            //If no book was found return error
+            if (books.Count == 0) return NotFound(new Error("Book Not Found", $"No book was found with this Name: {Name}."));
+            
+            //Otherwise, return the book
             return Ok(books);
         }
 
@@ -79,7 +105,7 @@ namespace BookSamsysAPI.Controllers
         [HttpPost]
         public async Task<IActionResult> AddBook(AddBookRequest addBook)
         {
-
+            //Create a book entity
             var book = new Book()
             {
                 Id = Guid.NewGuid(),
@@ -89,24 +115,32 @@ namespace BookSamsysAPI.Controllers
                 Price = addBook.Price
             };
 
+            //Get all books from DB
             var books = await dbContext.Books.ToListAsync();
 
+            //If there's a book with that ISBN return error 
             foreach (var book1 in books)
             {
                 if(book1.ISBN == book.ISBN) return BadRequest(new Error("ISBN already used", $"The ISBN must be unique and there is a book that has already this ISBN: {book.ISBN}."));
             }
+
+            //If the price of the book is negative return error
             if (book.Price < 0) return BadRequest(new Error("Negative Price","The price cannot be less than 0."));
+            
+            //If the ISBN has not 13 digitis return error
             if(book.ISBN.Length != 13) return BadRequest(new Error("Wrong ISBN", "The ISBN must be 13 digits"));
            
-
+            //Add book to the context
             await dbContext.Books.AddAsync(book);
+
+            //Save changes on DB
             await dbContext.SaveChangesAsync();
 
             return Ok(book);
         }
 
-        // PUT: api/books
-        [HttpPut("isbn/{ISBN}")]
+        // PUT: api/books/{ISBN}
+        [HttpPut("{ISBN}")]
         public async Task<IActionResult> UpdateBook(string ISBN, UpdateBookRequest updateBook)
         {
             //If the ISBN has not 13 digits return errpr
@@ -144,6 +178,33 @@ namespace BookSamsysAPI.Controllers
             return NotFound(new Error("Book Not Found", $"No book was found with this ISBN: {ISBN}."));
 
  
+        }
+
+        // DELETE: api/books/{isbn}
+        [HttpDelete]
+        [Route("{ISBN}")]
+        public async Task<IActionResult> deleteBook(string ISBN)
+        {
+            //If the ISBN has not 13 digits return errpr
+            if (ISBN.Length != 13) return BadRequest(new Error("Wrong ISBN", "The ISBN must has 13 digits"));
+
+            //Get all books from DB
+            var dbBooks = await dbContext.Books.ToListAsync();
+
+            //Find the book with the inputed ISBN
+            foreach (Book entry in dbBooks)
+            {
+                //If book was found delete it
+                if (entry.ISBN == ISBN)
+                {
+                    dbContext.Remove(entry);
+                    await dbContext.SaveChangesAsync();
+
+                    return Ok();
+                }
+            }
+            //If any book was found return error
+            return NotFound(new Error("Book Not Found", $"No book was found with this ISBN: {ISBN}."));
         }
 
     }
--- BookSamsysAPI/obj/Debug/net8.0/BookSamsysAPI.AssemblyInfo.cs
+++ BookSamsysAPI/obj/Debug/net8.0/BookSamsysAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookSamsysAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+212063a212f4b7dae43aa444b1140071e6aabe76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d76caf8ef76ab3725f05241fc69f81b5bc1a953")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookSamsysAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookSamsysAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
