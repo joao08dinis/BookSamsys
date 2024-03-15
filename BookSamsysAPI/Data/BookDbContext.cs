eed0fc02b17f7d927808e13b2dea04efd44f0aaa
Joao Laptop <97002278+joao08dinis@users.noreply.github.com>
2024-03-15 16:14:14 +0000

    Setup Project

---
+++ BookSamsysAPI/Data/BookDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BookSamsysAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookSamsysAPI.Data
+{
+    public class BookDbContext : DbContext
+    {
+        public BookDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ BookSamsysAPI/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace BookSamsysAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public long ISBN { get; set; }
+        public string Author { get; set; }
+        public float Price { get; set; }
+        public Book()
+        {
+
+        }
+    }
+
+}
