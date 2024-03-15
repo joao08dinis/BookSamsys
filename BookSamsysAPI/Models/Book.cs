f34aa2e682e7164dc65436a178ada04f1c1356b9
Joao Laptop <97002278+joao08dinis@users.noreply.github.com>
2024-03-15 18:13:26 +0000

    Update Controller
    Add method to create a book

--- BookSamsysAPI/Models/Book.cs
+++ BookSamsysAPI/Models/Book.cs
@@ -1,10 +1,22 @@
-﻿namespace BookSamsysAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookSamsysAPI.Models
 {
     public class Book
     {
+<<<<<<< Updated upstream
         public int Id { get; set; }
         public long ISBN { get; set; }
+=======
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+>>>>>>> Stashed changes
         public string Author { get; set; }
+        [Required]
         public float Price { get; set; }
         public Book()
         {
--- BookSamsysAPI/obj/Debug/net8.0/BookSamsysAPI.AssemblyInfo.cs
+++ BookSamsysAPI/obj/Debug/net8.0/BookSamsysAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookSamsysAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f83d71fd53bf985653f898816e6c765bb95a0e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+284099cbf50151bb5482bc090363e57ac43af2dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookSamsysAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookSamsysAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
