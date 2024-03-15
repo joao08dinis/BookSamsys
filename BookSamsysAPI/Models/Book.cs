080a023b0f3e573ff7f15dae9d1a9f6147f4388a
Joao Laptop <97002278+joao08dinis@users.noreply.github.com>
2024-03-15 16:39:55 +0000

    Add Migration and Update Book on Models
    On this commit, I add a migration and update my model class Book, on the previous commit the name attribute was missing.

--- BookSamsysAPI/Models/Book.cs
+++ BookSamsysAPI/Models/Book.cs
@@ -3,6 +3,7 @@
     public class Book
     {
         public int Id { get; set; }
+        public string Name { get; set; }
         public long ISBN { get; set; }
         public string Author { get; set; }
         public float Price { get; set; }
